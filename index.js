dc4b84067dbbec5996bedda3d97e2ba0662d0581
Dimitar Mirchev <dimitar.mirchev96@gmail.com>
2015-06-03 13:29:41 +0300

    path removed

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var morgan = require('morgan');
 var fs = require('fs');
-var path = require('path');
 
 module.exports = function(plasma, dna) {
   plasma.on(dna.reactOn || 'ExpressServer', function(c){
     var app = c.data;
 	if(dna.logFile){
-		// create a write stream (in append mode)
-		var accessLogStream = fs.createWriteStream(path.join(__dirname, dna.logFile), {flags: 'a'})
+		var accessLogStream = fs.createWriteStream(dna.logFile, {flags: 'a'})
 	}
 	app.use(morgan(dna.options, {stream: accessLogStream}));
   });
